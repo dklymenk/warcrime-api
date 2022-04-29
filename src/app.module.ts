680d5cdd3303e1f452b80bc9960dabba4498175b
Dmytro Klymenko <klimenkodr@gmail.com>
2022-04-29 20:48:44 +0300

    add reports crud boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
