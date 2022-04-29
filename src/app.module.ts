41199764dc5f13dd9579686a752a3b9783858345
Dmytro Klymenko <klimenkodr@gmail.com>
2022-04-30 01:14:37 +0300

    add adminjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
+import AdminJS from 'adminjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AdminModule } from '@adminjs/nestjs';
+import { Database, Resource } from '@adminjs/prisma';
+import { PrismaClient } from '@prisma/client';
+import { DMMFClass } from '@prisma/client/runtime';
+
+AdminJS.registerAdapter({ Database, Resource });
+
+const prisma = new PrismaClient();
+const dmmf = (prisma as any)._dmmf as DMMFClass;
 
 @Module({
-  imports: [ReportsModule, PrismaModule],
+  imports: [
+    ReportsModule,
+    PrismaModule,
+    AdminModule.createAdmin({
+      adminJsOptions: {
+        rootPath: '/admin',
+        resources: [
+          {
+            resource: { model: dmmf.modelMap.Report, client: prisma },
+            options: {},
+          },
+        ],
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
