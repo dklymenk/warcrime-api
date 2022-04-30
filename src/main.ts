3d3f81ccd0c372b1db5fb0df1e99ac809f10536b
Dmytro Klymenko <klimenkodr@gmail.com>
2022-04-30 16:06:53 +0300

    move port config to .env

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || '3000');
 }
 bootstrap();
