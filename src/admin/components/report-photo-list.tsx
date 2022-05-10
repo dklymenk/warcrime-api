f2d2982a78dbea24a7a1451258d8e38f2ae4fe95
Dmytro Klymenko <klimenkodr@gmail.com>
2022-05-10 23:36:47 +0300

    add image download button to admin component

--- src/admin/components/report-photo-list.tsx
+++ src/admin/components/report-photo-list.tsx
@@ -1,8 +1,23 @@
 import { BasePropertyProps } from 'adminjs';
+import styled from 'styled-components';
+const Container = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const ReportPhotoList = (props: BasePropertyProps) => {
   const { record, property } = props;
-  return <img width={400} src={record.params[property.path]}></img>;
+  return (
+    <Container>
+      <a download href={record.params[property.path]}>
+        <img width={360} src={record.params[property.path]}></img>
+      </a>
+      <a download href={record.params[property.path]}>
+        ⬇️ DOWNLOAD
+      </a>
+    </Container>
+  );
 };
 
 export default ReportPhotoList;
--- src/admin/components/report-photo-show.tsx
+++ src/admin/components/report-photo-show.tsx
@@ -1,11 +1,23 @@
 import { BasePropertyProps } from 'adminjs';
 import { ValueGroup } from '@adminjs/design-system';
+import styled from 'styled-components';
+const Container = styled('div')`
+  display: flex;
+  flex-direction: column;
+`;
 
 const ReportPhotoShow = (props: BasePropertyProps) => {
   const { record, property } = props;
   return (
     <ValueGroup label={property.label}>
-      <img width={800} src={record.params[property.path]}></img>
+      <Container>
+        <a download href={record.params[property.path]}>
+          <img width={800} src={record.params[property.path]}></img>
+        </a>
+        <a download href={record.params[property.path]}>
+          ⬇️ DOWNLOAD
+        </a>
+      </Container>
     </ValueGroup>
   );
 };
