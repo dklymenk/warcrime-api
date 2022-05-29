ec8f806b5b5cabb9b55c408f1704429e8154ea9d
Dmytro Klymenko <klimenkodr@gmail.com>
2022-05-29 13:27:47 +0300

    increase json body size

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { json } from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bodyParser: false,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useStaticAssets(join(__dirname, '..', 'public'), {
     setHeaders: (res) => res.setHeader('Access-Control-Allow-Origin', '*'),
   });
+  app.use(json({ limit: '50mb' }));
   app.enableCors();
   await app.listen(process.env.PORT || '3000');
 }
