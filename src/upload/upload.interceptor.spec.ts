4df1cec7be9eb8a6cdac7944c71eb605c433b04b
Dmytro Klymenko <klimenkodr@gmail.com>
2022-06-19 16:02:31 +0300

    remove upload.interceptor.spec.ts

--- src/upload/upload.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UploadInterceptor } from './upload.interceptor';
-
-describe('UploadInterceptor', () => {
-  it('should be defined', () => {
-    expect(new UploadInterceptor()).toBeDefined();
-  });
-});
