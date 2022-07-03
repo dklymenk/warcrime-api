5bb1b7e5bda637cef0bbf5cb866a49758d52ed2d
Dmytro Klymenko <klimenkodr@gmail.com>
2022-07-03 13:24:40 +0300

    avoid admin init in app e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppController } from 'src/app.controller';
+import { AppService } from 'src/app.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  // TODO
-  jest.setTimeout(90000);
-
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      controllers: [AppController],
+      providers: [AppService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
