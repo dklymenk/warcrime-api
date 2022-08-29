ecc11177aa505b826d729b29f3d76c28cea40944
Dmytro Klymenko <klimenkodr@gmail.com>
2022-08-29 10:35:50 +0300

    cleanup

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,19 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { extension } from 'mime-types';
-import { extname } from 'path';
 
 @Injectable()
 export class UploadService {
-  getNewFilename(filename: string): string {
-    const name = filename.split('.')[0];
-    const extension = extname(filename);
-    const randomName = Array(4)
-      .fill(null)
-      .map(() => Math.round(Math.random() * 16).toString(16))
-      .join('');
-    return `${name}-${randomName}${extension}`;
-  }
-
   generateFilename(mime: string): string {
     const extname = extension(mime);
     const randomName = Array(4)
